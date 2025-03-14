c660aa0f0b5ca1c039187824eb693f4894950f9a
Anish Kumar <akumar@dare.global>
2025-03-14 15:18:33 +0000

    Add examples

--- output.tf
+++ output.tf
@@ -7,3 +7,13 @@ output "bucket_arn" {
   value       = aws_s3_bucket.main.arn
   description = "The Amazon Resource Name (ARN) of the created S3 bucket."
 }
+
+output "webiste_domain" {
+  value       = aws_s3_bucket_website_configuration.main.website_domain
+  description = "The domain of the website endpoint."
+}
+
+output "website_endpoint " {
+  value       = aws_s3_bucket_website_configuration.main.website_endpoint
+  description = "Website Endpoint."
+}
